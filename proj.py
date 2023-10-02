6e56a3bac6b13a132121216ac5d373f57aa7902d
Carlos Esteves <cmdesteves@gmail.com>
2023-10-02 20:21:20 +0100

    Add first week checkpoint (check if in the future more variables are needed)

--- proj.py
+++ proj.py
@@ -11,6 +11,8 @@ with open('easy/easy_1.json', 'r') as input_json:
 # Create a MiniZinc data file
 with open('model_data.dzn', 'w') as dataFile:
     dataFile.write(f"numRequests = {len(data['patients'])};\n")
+    dataFile.write(f"numVehicles = {len(data['vehicles'])};\n")
+    dataFile.write(f"maxCategoryVehicle = {len(max(data['vehicles'], key= lambda x: len(x['canTake']))['canTake'])};\n")
     # Write Same Vehicle Backward
     dataFile.write(f"sameVehicleBackward = {'true' if data['sameVehicleBackward'] else 'false'};\n")
     # Write Max Wait Time
@@ -23,16 +25,27 @@ with open('model_data.dzn', 'w') as dataFile:
     dataFile.write(f"dest = {list(map(lambda x: x['destination'], data['patients']))};\n")
     # Write return points
     dataFile.write(f"ret = {list(map(lambda x: x['end'], data['patients']))};\n")
-    # Write sests ocuppied by patient
+    # Write seats ocuppied by patient
     dataFile.write(f"l = {list(map(lambda x: x['load'], data['patients']))};\n")
-    # Write sests ocuppied by patient
+    # Write appointment hour
     dataFile.write(f"u = {list(map(lambda x: stringHoursToMinute(x['rdvTime']), data['patients']))};\n")
-    # Write sests ocuppied by patient
+    # Write appointment duration
     dataFile.write(f"d = {list(map(lambda x: stringHoursToMinute(x['rdvDuration']), data['patients']))};\n")
-    # Write sests ocuppied by patient
+    # Write wait time
     dataFile.write(f"p = {list(map(lambda x: stringHoursToMinute(data['maxWaitTime']), data['patients']))};\n")
-    # Write sests ocuppied by patient
+    # Write patient category
     dataFile.write(f"c = {list(map(lambda x: x['category'], data['patients']))};\n")
+
+    # Write vehicles capacity
+    dataFile.write(f"k = {list(map(lambda x: x['capacity'], data['vehicles']))};\n")
+    # Write patient category
+    #dataFile.write(f"C = {list(map(lambda x: (x['canTake']), data['vehicles']))};\n")
+    dataFile.write("C = [|")
+    for v in data['vehicles']:
+        for can in v['canTake']:
+            dataFile.write(f"{can},")
+        dataFile.write('|')
+    dataFile.write("];")
     
 
 
