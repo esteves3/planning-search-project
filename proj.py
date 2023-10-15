245fb9b104bfe8130e5badb4de223dc07b25ab30
Carlos Esteves <cmdesteves@gmail.com>
2023-10-15 18:23:52 +0100

    wip

--- proj.py
+++ proj.py
@@ -4,7 +4,7 @@ from minizinc import Instance, Model, Solver
 def stringHoursToMinute(str):
     return int(str.split('h')[0]) * 60 + int(str.split('h')[1])
 
-model = Model("./model2.mzn")
+model = Model("./model3.mzn")
 gecode = Solver.lookup("gecode")
 
 instance = Instance(gecode, model)
@@ -13,43 +13,81 @@ instance = Instance(gecode, model)
 with open('easy/easy_1.json', 'r') as input_json:
     data = json.load(input_json)
 
-instance['numRequests'] = len(data['patients'])
+idsA = []
+org = []
+dst = []
+srv = []
+l = []
+rdv = []
+drdv = []
+maxw = []
+c = [] 
+for patient in data['patients']:
+    if patient["start"] != -1:
+        idsA.append(int(patient["id"]))
+        org.append(int(patient["start"]))
+        dst.append(int(patient["destination"]))
+        srv.append(stringHoursToMinute(patient["srvDuration"]))
+        l.append(int(patient["load"]))
+        rdv.append(stringHoursToMinute(patient["rdvTime"]))
+        drdv.append(stringHoursToMinute(patient["rdvDuration"]))
+        maxw.append(stringHoursToMinute(data['maxWaitTime']))
+        c.append(patient["category"])
+
+    if patient["end"] != -1:
+        idsA.append(int(patient["id"]))
+        org.append(int(patient["destination"]))
+        dst.append(int(patient["end"]))
+        srv.append(stringHoursToMinute(patient["srvDuration"]))
+        l.append(int(patient["load"]))
+        rdv.append(stringHoursToMinute(patient["rdvTime"]))
+        drdv.append(stringHoursToMinute(patient["rdvDuration"]))
+        maxw.append(stringHoursToMinute(data['maxWaitTime']))
+        c.append(patient["category"])
+
+
+instance['numActivities'] = len(idsA)
 instance['numVehicles'] = sum(list(map(lambda x: len(x['availability']), data['vehicles'])))
 instance['distMatrixLen'] = len(data['distMatrix'])
 instance['sameVehicleBackward'] = data['sameVehicleBackward']
 instance['maxWaitTime'] = stringHoursToMinute(data['maxWaitTime'])
-instance['start'] = list(map(lambda x: int(x['start']), data['patients']))
-instance['dest'] = list(map(lambda x: int(x['destination']), data['patients']))
-instance['ret'] = list(map(lambda x: int(x['end']), data['patients']))
-instance['l'] = list(map(lambda x: int(x['load']), data['patients']))
-instance['u'] = list(map(lambda x: stringHoursToMinute(x['rdvTime']), data['patients']))
-instance['d'] = list(map(lambda x: stringHoursToMinute(x['rdvDuration']), data['patients']))
-instance['srv'] = list(map(lambda x: stringHoursToMinute(x['srvDuration']), data['patients']))
-instance['p'] = list(map(lambda x: stringHoursToMinute(data['maxWaitTime']), data['patients']))
-instance['c'] = list(map(lambda x: int(x['category']), data['patients']))
+instance['idsA'] = idsA
+instance['org'] = org
+instance['dst'] = dst
+instance['srv'] = srv
+instance['l'] = l
+instance['rdv'] = rdv
+instance['drdv'] = drdv
+instance['maxw'] = maxw
+instance['c'] = c
 
+idsV = []
 k = []
 C = []
-vas = []
-vae = []
-vstart = []
-vend = []
+sd = []
+ed = []
+savail = []
+eavail = []
 for v in data['vehicles']:
     for a in v['availability']:
+        idsV.append(int(v['id']))
         k.append(int(v['capacity']))
-        vas.append(stringHoursToMinute(a.split(':')[0]))
-        vae.append(stringHoursToMinute(a.split(':')[1]))
-        vstart.append(int(v['start']))
-        vend.append(int(v['end']))
+        savail.append(stringHoursToMinute(a.split(':')[0]))
+        eavail.append(stringHoursToMinute(a.split(':')[1]))
+        sd.append(int(v['start']))
+        ed.append(int(v['end']))
         C.append(set(list(map(lambda x: int(x), v['canTake']))))
 
 
+instance['idsV'] = idsV
 instance['k'] = k
 instance['C'] = C
-instance['vas'] = vas
-instance['vae'] = vae
-instance['vstart'] = vstart
-instance['vend'] = vend
+instance['sd'] = sd
+instance['ed'] = ed
+instance['savail'] = savail
+instance['eavail'] = eavail
+
+
 instance['T'] = data['distMatrix']
 
 
---
+++ projBackup.py
@@ -0,0 +1,59 @@
+import json
+from minizinc import Instance, Model, Solver
+
+def stringHoursToMinute(str):
+    return int(str.split('h')[0]) * 60 + int(str.split('h')[1])
+
+model = Model("./model2.mzn")
+gecode = Solver.lookup("gecode")
+
+instance = Instance(gecode, model)
+
+# Load the JSON data from a file
+with open('easy/easy_1.json', 'r') as input_json:
+    data = json.load(input_json)
+
+instance['numRequests'] = len(data['patients'])
+instance['numVehicles'] = sum(list(map(lambda x: len(x['availability']), data['vehicles'])))
+instance['distMatrixLen'] = len(data['distMatrix'])
+instance['sameVehicleBackward'] = data['sameVehicleBackward']
+instance['maxWaitTime'] = stringHoursToMinute(data['maxWaitTime'])
+instance['start'] = list(map(lambda x: int(x['start']), data['patients']))
+instance['dest'] = list(map(lambda x: int(x['destination']), data['patients']))
+instance['ret'] = list(map(lambda x: int(x['end']), data['patients']))
+instance['l'] = list(map(lambda x: int(x['load']), data['patients']))
+instance['u'] = list(map(lambda x: stringHoursToMinute(x['rdvTime']), data['patients']))
+instance['d'] = list(map(lambda x: stringHoursToMinute(x['rdvDuration']), data['patients']))
+instance['srv'] = list(map(lambda x: stringHoursToMinute(x['srvDuration']), data['patients']))
+instance['p'] = list(map(lambda x: stringHoursToMinute(data['maxWaitTime']), data['patients']))
+instance['c'] = list(map(lambda x: int(x['category']), data['patients']))
+
+k = []
+C = []
+vas = []
+vae = []
+vstart = []
+vend = []
+for v in data['vehicles']:
+    for a in v['availability']:
+        k.append(int(v['capacity']))
+        vas.append(stringHoursToMinute(a.split(':')[0]))
+        vae.append(stringHoursToMinute(a.split(':')[1]))
+        vstart.append(int(v['start']))
+        vend.append(int(v['end']))
+        C.append(set(list(map(lambda x: int(x), v['canTake']))))
+
+
+instance['k'] = k
+instance['C'] = C
+instance['vas'] = vas
+instance['vae'] = vae
+instance['vstart'] = vstart
+instance['vend'] = vend
+instance['T'] = data['distMatrix']
+
+
+result = instance.solve()
+
+print( result['vf'])
+print( result['vb'])
