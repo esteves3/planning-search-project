15d140c6365901db7d6736985d6026ac155883ab
Carlos Esteves <cmdesteves@gmail.com>
2023-10-16 18:11:30 +0100

    wip

--- proj.py
+++ proj.py
@@ -1,16 +1,28 @@
+import sys
 import json
+import math
 from minizinc import Instance, Model, Solver
 
+print("STARTING...")
 def stringHoursToMinute(str):
     return int(str.split('h')[0]) * 60 + int(str.split('h')[1])
+
+def minuteToStringHours(minutes):
+    hours, remainder_minutes = divmod(minutes, 60)
+    return f"{hours:02d}h{remainder_minutes:02d}"
 
 model = Model("./model3.mzn")
 gecode = Solver.lookup("gecode")
 
 instance = Instance(gecode, model)
 
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+print("Reading from file: " + inputfile)
 # Load the JSON data from a file
-with open('easy/easy_1.json', 'r') as input_json:
+with open(inputfile, 'r') as input_json:
     data = json.load(input_json)
 
 idsA = []
@@ -21,7 +33,9 @@ l = []
 rdv = []
 drdv = []
 maxw = []
-c = [] 
+c = []
+isForward = []
+
 for patient in data['patients']:
     if patient["start"] != -1:
         idsA.append(int(patient["id"]))
@@ -33,6 +47,7 @@ for patient in data['patients']:
         drdv.append(stringHoursToMinute(patient["rdvDuration"]))
         maxw.append(stringHoursToMinute(data['maxWaitTime']))
         c.append(patient["category"])
+        isForward.append(1)
 
     if patient["end"] != -1:
         idsA.append(int(patient["id"]))
@@ -44,6 +59,7 @@ for patient in data['patients']:
         drdv.append(stringHoursToMinute(patient["rdvDuration"]))
         maxw.append(stringHoursToMinute(data['maxWaitTime']))
         c.append(patient["category"])
+        isForward.append(0)
 
 
 instance['numActivities'] = len(idsA)
@@ -60,6 +76,7 @@ instance['rdv'] = rdv
 instance['drdv'] = drdv
 instance['maxw'] = maxw
 instance['c'] = c
+instance['isForward'] = isForward
 
 idsV = []
 k = []
@@ -91,7 +108,30 @@ instance['eavail'] = eavail
 instance['T'] = data['distMatrix']
 
 
+print("Variables defined, starting model...")
 result = instance.solve()
+print("Model solved")
 
-print( result['vf'])
-print( result['vb'])
+
+print("Defining output...")
+outputData = {}
+outputList = []
+
+for i in range(len(idsA)):
+    if result['S'][i] == 1:
+        outputList.append({
+            'id': idsA[i],
+            'isForward': isForward[i],
+            'startHour': minuteToStringHours(result['sA'][i]),
+            'duration': minuteToStringHours(result['dA'][i]),
+            'endHour': minuteToStringHours(result['eA'][i]),
+            'vehicle': result['vA'][i]
+        })
+
+outputData['trips'] = outputList
+
+print("Writing output to " + outputfile)
+with open(outputfile, 'w') as output:
+    output.write(json.dumps(outputData, indent=4))
+
+print("Done")
